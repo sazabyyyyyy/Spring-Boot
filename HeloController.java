7e3bbb0b531a3dc2c263f8676e4fc52a361a8c31
takuya <starfish1138jp@gmail.com>
2018-09-13 00:33:34 +0900

    form

--- HeloController.java
+++ HeloController.java
@@ -1,8 +1,10 @@
 package com.kaneko.springboot;
 
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author Takuya Kaneko
@@ -10,66 +12,30 @@ import org.springframework.web.bind.annotation.RestController;
  *その番号のデータがJSON形式で出力される
  */
 
-@RestController
+@Controller
 public class HeloController {
 
-	String[] names ={"kaneko","kimura","kaneko","mnoriok","takuya"
-	};
+	@RequestMapping(value="/",method=RequestMethod.GET)
+	public ModelAndView index(ModelAndView mav){
 
-	String[] mails= {"stassiks.com","kankoe@ko.jp","eioko@sk.kp","hakpsoko@dd,kp"};
+			mav.addObject("msg","お名前を書いて送信してください");
+			mav.setViewName("index");
+			return mav;
+			}
 
-
-
-
-	//{num}がパス変数の指定
-	@RequestMapping("/{id}")
-	//@PathVariableのあとの引数がパス変数によって値を渡されるもの
-	public  DataObject index(@PathVariable int id) {
-
-		return new DataObject(id,names[id],mails[id]);
-
-	}
-
-}
-class DataObject{
-	private int id;
-	private String name;
-	private String value;
-
-	//コンストラクタ
-	public DataObject(int id, String name, String value) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.value = value;
-	}
-
-	public int getId() {
-		return id;
-		}
-
-	public void setId(int id) {
-		this.id=id;
-	}
-
-	public String getName() {
-		return name;
-
-	}
-
-	public void setName(String name) {
-		this.name=name;
-	}
-
-	public String getValue() {
-		return value;
-	}
-
-	public void setValue(String value) {
-		this.value=value;
+	@RequestMapping(value="/",method=RequestMethod.POST)
+	public ModelAndView send(@RequestParam("text1")String str,ModelAndView mav) {
+		mav.addObject("msg","こんにちは"+str+"さん");
+		mav.addObject("value",str);
+		mav.setViewName("index");
+		return mav;
 	}
 
 
 
 
-}
+
+	}
+
+
+
